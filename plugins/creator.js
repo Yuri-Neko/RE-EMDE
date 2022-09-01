8ed07d3d842512cac21e02f4281d32d095517ab3
Kyouka Hashiba <91820453+Yuri-Neko@users.noreply.github.com>
2022-09-01 12:45:24 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,7 +2,7 @@ function handler(m) {
   
   const kontak = {
 	"displayName": 'My owner',
-	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('6283820073017@s.whatsapp.net')}\nitem1.TEL;waid=6283820073017:6283820073017\nitem1.X-ABLabel:Busy.\nURL;My Web: https://hi.rlxfly.my.id\nEMAIL;Email Owner: me@rlxfly.my.id\nORG: NOT A BOT + NO SAVE\nTEL;My number bot;waid=6287845646738:6287845646738\nEND:VCARD`
+	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;;;;\nFN:${conn.getName('6282112080081@s.whatsapp.net')}\nitem1.TEL;waid=6282112080081:6282112080081\nitem1.X-ABLabel:Busy.\nURL;My Web: https://kyouka-website.vercel.app/\nEMAIL;Email Owner: nekololi@gmail.com\nORG: NOT A BOT + NO SAVE\nTEL;My number bot;waid=6283825121214:6283825121214\nEND:VCARD`
 }
 
 conn.sendMessage(m.chat, { contacts: { contacts: [kontak] }}, { quoted: m })
